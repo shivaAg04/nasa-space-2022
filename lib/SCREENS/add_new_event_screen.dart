4b152d17c1c077a363d42f65a134189acc59ba1d
shivaAg04 <shivashivank12345@gmail.com>
2022-10-02 20:00:39 +0530

    after adding event register screen

--- lib/SCREENS/add_new_event_screen.dart
+++ lib/SCREENS/add_new_event_screen.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/material.dart';
 
 class AddNewEventScreen extends StatelessWidget {
-  const AddNewEventScreen({Key? key}) : super(key: key);
+  AddNewEventScreen({Key? key}) : super(key: key);
+  TextEditingController titlecontroller = TextEditingController();
+  TextEditingController pricecontroller = TextEditingController();
+  TextEditingController descriptioncontroller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 60, 255, 236),
+        title: const Text(
+          "Add Events",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "enter value";
+                    } else {
+                      return null;
+                    }
+                  },
+                  controller: titlecontroller,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Colors.orange),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "enter value";
+                    } else {
+                      return null;
+                    }
+                  },
+                  controller: pricecontroller,
+                  decoration: InputDecoration(
+                    labelText: 'Price',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Colors.orange),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/SCREENS/events_screen.dart
+++ lib/SCREENS/events_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nasa_space2022/SCREENS/add_new_event_screen.dart';
 
 class EventsScreen extends StatelessWidget {
   const EventsScreen({Key? key}) : super(key: key);
@@ -8,7 +9,19 @@ class EventsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddNewEventScreen()),
+                );
+              },
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ))
+        ],
         backgroundColor: const Color.fromARGB(255, 60, 255, 236),
         title: const Text(
           "Events",
